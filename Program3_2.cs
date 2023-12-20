3a23011ed055a53ed9008aa540f16857e194c91a
Ruden <stocky67@mail.ru>
2023-12-20 23:49:01 +0500

    done 3_2

---
+++ Program3_2.cs
@@ -0,0 +1,24 @@
+﻿int[] arr = new int[10];
+Random rand = new Random();
+for (int i = 0; i < 10; i++)
+{
+    arr[i] = rand.Next(1, 101);
+}
+
+Console.WriteLine("Массив:");
+foreach(int num in arr)
+{
+    Console.Write(num + " ");
+}
+
+int evenCount = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        evenCount++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных чисел: " + evenCount);
